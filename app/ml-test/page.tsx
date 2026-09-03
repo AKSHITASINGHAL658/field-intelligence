eb04a6984903e6a444353727fc94d29123ff657a
kmanimadhav1-bit <mani.madhav@sikgen.ai>
2026-09-03 23:26:22 +0530

    Corrected ONNX File

---
+++ app/ml-test/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useState } from "react";
+import { classifyImage } from "@/lib/plantClassifier";
+
+export default function MLTestPage() {
+
+  const [result, setResult] = useState<any>(null);
+
+  async function handleImage(
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
+
+    const file = event.target.files?.[0];
+
+    if (!file) return;
+
+    const image = new Image();
+
+    image.src = URL.createObjectURL(file);
+
+    image.onload = async () => {
+
+      const prediction =
+        await classifyImage(image);
+
+      setResult(prediction);
+
+      URL.revokeObjectURL(image.src);
+    };
+  }
+
+  return (
+    <main
+      style={{
+        padding: 40,
+        fontFamily: "Arial"
+      }}
+    >
+
+      <h1>
+        Plant ML Test
+      </h1>
+
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImage}
+      />
+
+      {result && (
+        <pre
+          style={{
+            marginTop: 30
+          }}
+        >
+          {JSON.stringify(
+            result,
+            null,
+            2
+          )}
+        </pre>
+      )}
+
+    </main>
+  );
+}
